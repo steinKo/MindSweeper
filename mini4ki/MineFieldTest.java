7c973cab7dd2b83f548d8ccd91214ff561983866
stein <stein_korsveien@msn.com>
2016-03-08 09:28:56 +0100

    Splited methods in the MineField class

---
+++ mini4ki/MineFieldTest.java
@@ -0,0 +1,13 @@
+
+
+import org.junit.Test;
+
+public class MineFieldTest {
+	MineField mineField =new MineField();
+
+	@Test
+	public void testShow() {
+		mineField.show();
+	}
+
+}
---
+++ mini4ki/RankingTest.java
@@ -0,0 +1,26 @@
+
+
+import org.junit.Test;
+
+public class RankingTest {
+
+	Ranking ranking =new Ranking(); 
+	
+	@Test
+	
+	public void testShow() {
+	
+		ranking.show();
+		
+	}
+@Test
+	
+	public void testRecordName() {
+	
+		ranking.recordName(0);
+		
+	}
+	
+	
+
+}
